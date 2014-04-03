41db09061a1bdf908d7e56ba9ba7d7f6c16730c1
Masahiro TAMURA <tamoot@gmail.com>
2014-04-03 22:08:47 +0900

    add "reStructuredText" format

--- lib/display_readme.rb
+++ lib/display_readme.rb
@@ -1,6 +1,7 @@
 class DisplayReadme < Redmine::Hook::ViewListener
 
   @@markdown_ext = %w(.markdown .mdown .mkdn .md .mkd .mdwn .mdtxt .mdtext .text)
+  @@rst_ext = %w(.rst .rest)
 
   def view_repositories_show_contextual(context)
 
@@ -22,6 +23,8 @@ class DisplayReadme < Redmine::Hook::ViewListener
     formatter_name = ''
     if @@markdown_ext.include?(File.extname(file.path))
       formatter_name = Redmine::WikiFormatting.format_names.find { |name| name =~ /markdown/i }
+    elsif @@rst_ext.include?(File.extname(file.path))
+      formatter_name = Redmine::WikiFormatting.format_names.find { |name| name =~ /reStructuredText/i }
     end
 
     formatter = Redmine::WikiFormatting.formatter_for(formatter_name).new(raw_readme_text)
